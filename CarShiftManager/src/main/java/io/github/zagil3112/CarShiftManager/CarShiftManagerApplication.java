3e9bfddab0ec8d0ae93bd6a209bdba403c1d75ab
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2023-03-27 00:47:12 -0500

    new SB files
    new SB files

---
+++ CarShiftManager/src/main/java/io/github/zagil3112/CarShiftManager/CarShiftManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.zagil3112.CarShiftManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShiftManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShiftManagerApplication.class, args);
+	}
+
+}
---
+++ CarShiftManager/src/test/java/io/github/zagil3112/CarShiftManager/CarShiftManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zagil3112.CarShiftManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShiftManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
