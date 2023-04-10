9d68ccd8db5a87d20483dcf3de19781865b36453
Sebastian Augusto Zapata Gil <sebasazg@gmail.com>
2023-04-10 02:00:30 -0500

    forbidden

--- platzi_market-clase-34-autenticaci-n/src/main/java/com/platzi/market/web/security/SecurityConfig.java
+++ platzi_market-clase-34-autenticaci-n/src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -22,9 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-                //.authorizeRequests().antMatchers("/**/authenticate").permitAll()
-        //                .anyRequest().authenticated();
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                        .anyRequest().authenticated();
     }
 
     @Override
